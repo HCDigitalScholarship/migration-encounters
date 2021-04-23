aae33a72751198c07b40fe5abc2873daf9e26c80
ajanco <apjanco@gmail.com>
2021-04-23 10:21:10 -0400

    let's go Cormorant

--- build.py
+++ build.py
@@ -3,19 +3,22 @@ from main import index
 import shutil
 from pathlib import Path 
 
-site_path = Path.cwd() / 'site'
-if not site_path.exists():
-    site_path.mkdir(parents=True, exist_ok=True)
 
 def build_index():
     page = index(Request)
     (site_path / 'index.html').write_bytes(page.body)
 
 if __name__ == '__main__':
-    # copy assets to site 
+    # Create site directory
+    site_path = Path.cwd() / 'site'
+    if not site_path.exists():
+        site_path.mkdir(parents=True, exist_ok=True)
+
+    # Copy assets to site directory
     if not (site_path / 'assets').exists():
         shutil.copytree((Path.cwd() / 'assets'), (site_path / 'assets')) 
     else:
         shutil.rmtree((site_path / 'assets'))
         shutil.copytree((Path.cwd() / 'assets'), (site_path / 'assets')) 
+
     build_index()
