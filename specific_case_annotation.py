8ea3ad7c4c651d2aea701ae832aa476623c15add
hikaru-jitsukawa <85036185+hikaru-jitsukawa@users.noreply.github.com>
2021-06-11 11:52:19 -0400

    Update specific_case_annotation.py

--- specific_case_annotation.py
+++ specific_case_annotation.py
@@ -2,7 +2,7 @@ import httpx
 import re
 
 
-def hypothesisgrabber(interview_name, offset):
+def hypothesis_grabber(interview_name, offset):
     """This program takes two variables, the interview name and the offset. Starting at the offset,
      it returns the next 200 annotations (or as many as available) as a list."""
 
@@ -14,10 +14,10 @@ def hypothesisgrabber(interview_name, offset):
     return grabbed_annotations
 
 
-def regex_search_ignore_space(pattern_text, string):
+def search_ignore_space(pattern_text, string):
     """This program takes two variables, a string to search (pattern_text) within a larger string (string). It matches
-    (pattern_text) within (string), ignoring all whitespace characters [^ \t\n\r\f\v]. It then returns the matched 
-    string with whitespace characters included, as well as the start and end character locations of 
+    (pattern_text) within (string), ignoring all whitespace characters [^ \t\n\r\f\v]. It then returns the matched
+    string with whitespace characters included, as well as the start and end character locations of
     (pattern_text) in (string)"""
 
     no_spaces = ''
@@ -33,6 +33,7 @@ def regex_search_ignore_space(pattern_text, string):
     for char in whitespace_characters:
         pattern_text = pattern_text.replace(char, '')
 
+    #
     matched_string_start = no_spaces.find(pattern_text)
     matched_string_end = matched_string_start + len(pattern_text)
 
@@ -40,7 +41,7 @@ def regex_search_ignore_space(pattern_text, string):
     # of the found string in the spaceless string
     # (as we have searched in it).
     start = char_positions[matched_string_start]  # in the original string
-    end = char_positions[matched_string_end]-1  # in the original string
+    end = char_positions[matched_string_end] - 1  # in the original string
     matched_string = string[start:end]
 
     # the match WITH spaces, and the start and end locations in the original string is returned.
@@ -60,7 +61,8 @@ def main():
         'https://api.hypothes.is/api/search?uri=https://www.migrationencounters.org/stories/' + interview_name +
         '&limit=1')
     datadict = data.json()
-    total_remaining_annotations = int(datadict.get("total"))
+    total_annotations = int(datadict.get("total"))
+    total_remaining_annotations = total_annotations
 
     # initializing our variables
     offset = 0
@@ -68,54 +70,65 @@ def main():
 
     # grabbing our annotations 200 at a time and appending them to annotations_list
     while total_remaining_annotations > 0:
-        grabbed_annotations = hypothesisgrabber(interview_name, offset)
+        grabbed_annotations = hypothesis_grabber(interview_name, offset)
         for n in range(0, len(grabbed_annotations)):
             annotations_list.append(grabbed_annotations[n])
         offset = offset + 200
         total_remaining_annotations = total_remaining_annotations - 200
 
-    # from the list of annotations, inputting the number of a specific annotation and pulling that specific
-    # annotation from the list of annotations
-    annotation_number = int(input("Enter number of annotation: "))
-    specific_annotation = annotations_list[annotation_number - 1]
-
-    # creating a dictionary for the relevant information from the annotation
-    result_dictionary = {}
-
-    # inputting the text of the annotation and the relevant tags to our dictionary
-    # TODO perhaps split each of these text labels and tags into separate entries.
-    annotation_text = specific_annotation.get('text')
-    result_dictionary['label_text'] = annotation_text
-    annotation_tags = specific_annotation.get('tags')
-    result_dictionary['label_tags'] = annotation_tags
-
-    # inputting the text being referenced to our dictionary
-    target = specific_annotation.get("target")
-    target_dictionary = target[0]
-    selector = target_dictionary.get('selector')
-    referenced_text = selector[2]
-    exact_text = referenced_text.get('exact')  # from the hypothesis api
-
-    # TODO put in a checker using the prefix and suffix
-    # defining the prefix and suffix of our exact text
-    prefix = referenced_text.get("prefix")  # from the hypothesis api
-    suffix = referenced_text.get("suffix")  # from the hypothesis api
-
     # TODO the following line only works with single word names.
     # retrieving the raw text of the interviews
-    raw_text_document = httpx.get('https://raw.githubusercontent.com/HCDigitalScholarship/migration-encounters/main'
-                                  '/data/' + interview_name[0].upper() + interview_name[1:] + '.json')
+    raw_text_document = httpx.get(
+        'https://raw.githubusercontent.com/HCDigitalScholarship/migration-encounters/main'
+        '/data/' + interview_name[0].upper() + interview_name[1:] + '.json')
     raw_text_document = raw_text_document.json()
     raw_text_document = raw_text_document.get('text')
 
-    string_location = regex_search_ignore_space(exact_text, raw_text_document)
-    result_dictionary['exact_text'] = string_location[0]
-    result_dictionary['start'] = string_location[1]
-    result_dictionary['end'] = string_location[2]
+    # creating a list for our final output
+    annotations = []
 
-    # printing our completed dictionary for a specific annotation
-    print(result_dictionary)
+    # appending each reformatted annotation to the final list.
+    for annotation_number in range(0, int(total_annotations)):
+        try:
+            specific_annotation = annotations_list[annotation_number - 1]
+
+            # creating a dictionary for the relevant information from the annotation
+            result_dictionary = {}
+
+            # inputting the text of the annotation and the relevant tags to our dictionary
+            # TODO perhaps split each of these text labels and tags into separate entries.
+            annotation_text = specific_annotation.get('text')
+            result_dictionary['label_text'] = annotation_text
+            annotation_tags = specific_annotation.get('tags')
+            result_dictionary['label_tags'] = annotation_tags
+
+            # finding the referenced text in the annotation
+            target = specific_annotation.get("target")
+            target_dictionary = target[0]
+            selector = target_dictionary.get('selector')
+            referenced_text = selector[2]
+            exact_text = referenced_text.get('exact')  # from the hypothesis api
+
+            # TODO put in a checker using the prefix and suffix
+            # defining the prefix and suffix of our exact text
+            prefix = referenced_text.get("prefix")  # from the hypothesis api
+            suffix = referenced_text.get("suffix")  # from the hypothesis api
+
+            # finding the location of the annotation in the larger text and adding to our dictionary
+            string_location = search_ignore_space(exact_text, raw_text_document)
+            result_dictionary['exact_text'] = string_location[0]
+            result_dictionary['start'] = string_location[1]
+            result_dictionary['end'] = string_location[2]
+
+            # appending our resultant dictionary to our list of annotations
+            annotations.append(result_dictionary)
+
+        except:
+            pass
+
+    print(annotations)
 
 
 if __name__ == '__main__':
     main()
+
