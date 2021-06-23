77fc29f954506eb3ef2df9f857f175ae9fd22e52
hikaru-jitsukawa <buksi.pi@gmail.com>
2021-06-23 15:17:27 -0400

    Reformatted Text for Readability

--- utils/filter_demo.py
+++ utils/filter_demo.py
@@ -16,4 +16,25 @@ if not filters:
 else:
     for n in range(0, len(filters)):
         hypothesis_annotations = hypothesis_annotations[hypothesis_annotations['label'].str.contains(filters[n])]
-    st.write(hypothesis_annotations)
+    for m in range(0, len(hypothesis_annotations)):
+        # Turning our relevant values into strings.
+        labels = hypothesis_annotations['label']
+        labels = labels.iloc[[m]].to_string()
+
+        name = hypothesis_annotations['name']
+        name = name.iloc[[m]].to_string()
+
+        text = hypothesis_annotations['text']
+        text = text.iloc[[m]].to_string()
+
+        # Removing unnecessary characters.
+        remove = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "\\n"]
+        for l in range(0, len(remove)):
+            labels = labels.replace(remove[l], "")
+            name = name.replace(remove[l], "")
+            text = text.replace(remove[l], "")
+
+        st.write(name)
+        st.write(labels)
+        st.write(text)
+        st.write("---")
