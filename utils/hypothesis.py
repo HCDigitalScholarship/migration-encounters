0253099d6765504c1e1b7d4eccf1a3cba0e9fa5a
hikaru-jitsukawa <buksi.pi@gmail.com>
2021-06-23 10:26:49 -0400

    Removing Spacy Ents

--- utils/hypothesis.py
+++ utils/hypothesis.py
@@ -343,8 +343,6 @@ def make_csv(data:List[dict]):
                 i['text'] = row['text'][i['start']:i['end']] 
                 df_data.append(i)
     df = pd.DataFrame(df_data)
-    drop = ['CARDINAL', 'DATE', 'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'MONEY', 'NORP', 'ORDINAL', 'ORG','PERCENT', 'PERSON', 'PRODUCT', 'QUANTITY', 'TIME', 'WORK_OF_ART']
-    df = df[~df['label'].isin(drop)]
     return df
 
 # Remove spacy ents
