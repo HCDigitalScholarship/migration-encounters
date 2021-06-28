861eee29a6e64787e312fc13cbd8986cb20153bd
hikaru-jitsukawa <buksi.pi@gmail.com>
2021-06-28 14:57:42 -0400

    Annotating and making minor formatting changes.

--- utils/filter_demo.py
+++ utils/filter_demo.py
@@ -1,41 +1,54 @@
 import streamlit as st
 import pandas as pd
 
+
 def nonempty_checker(index_themes, hypothesis_annotations):
-    nonempty_index_themes = []
+    """ This function takes two variables, a list of index themes and a CSV of annotations. It outputs
+    a list of lists, each of which gives us an index theme that appears in our annotations - and the
+    number of times this index theme occurs. """
+    nonempty_index_themes = []  # Initializing our output variables.
     number_of_occurrences = []
     for theme in range(0, len(index_themes)):
+        # Checking whether our index themes appear in the annotations.
         non_empty_checker = hypothesis_annotations[hypothesis_annotations['label'].str.contains(index_themes[theme])]
-        if non_empty_checker.empty:
+        if non_empty_checker.empty:  # ignoring the index theme if it doesn't appear in the annotations.
             continue
         else:
+            # appending our index theme to our finalized list, and taking the length of the matched CSV as
+            # the number of occurrences.
             nonempty_index_themes.append(index_themes[theme])
             number_of_occurrences.append(len(non_empty_checker))
 
     return [nonempty_index_themes, number_of_occurrences]
 
 
+# opening our CSV of hypothesis annotations - this notation only works on the deployed Streamlit platform.
+# locally, add "../" to the beginning of the filepath.
 hypothesis_annotations = pd.read_csv("utils/hypothesis_data.csv")
 
 index_themes = open("index_themes.txt").read().splitlines()
 
 index_themes = list(set(index_themes))  # removing duplicates
-filters = st.multiselect("Choose Labels", list(index_themes))
+filters = st.multiselect("Choose Labels", list(index_themes))  # creating an input field.
+st.write("---")  # including a horizontal line for readability.
 
-
-if not filters:
+if not filters:  # An error message for when there is no input.
     st.error("Please select at least one filter.")
 
 else:
-    pd.set_option('display.max_colwidth', None)
-    for n in range(0, len(filters)):
+    pd.set_option('display.max_colwidth', None)  # removing the character limit.
+
+    for n in range(0, len(filters)):  # finding which annotations match the inputted filters
+        # at each iteration, the hypothesis_annotations variable updates to only include the relevant terms
         hypothesis_annotations = hypothesis_annotations[hypothesis_annotations['label'].str.contains(filters[n])]
 
+    # running our checker for non-empty index themes.
     nonempty_index = nonempty_checker(index_themes, hypothesis_annotations)
+
+    # creating a sidebar for the non-empty labels.
     st.sidebar.title("These Labels Are Paired With:")
-    for entry in range(0, len(nonempty_index[0])):
+    for entry in range(0, len(nonempty_index[0])):  # writing each of our non-empty labels and their frequency.
         st.sidebar.write(nonempty_index[0][entry], nonempty_index[1][entry])
-    st.write("---")
 
     for m in range(0, len(hypothesis_annotations)):
         # Turning our relevant values into strings.
@@ -54,6 +67,8 @@ else:
             labels = labels.replace(remove[l], "")
             name = name.replace(remove[l], "")
             text = text.replace(remove[l], "")
+
+        # writing our final output
         st.write(name)
         st.write(labels)
         st.write(text)
--- utils/hypothesis.py
+++ utils/hypothesis.py
@@ -2,75 +2,75 @@ import httpx
 import re
 import spacy
 from rapidfuzz import process, fuzz
-from pathlib import Path 
-from tqdm import tqdm 
+from pathlib import Path
+from tqdm import tqdm
 import srsly
 import pandas as pd
 from typing import List
 
-names_files = {'Joseph':'joseph',
- 'Jesus A':'jesus_a',
- 'Erik':'erik',
- 'Abel':'abel',
- 'Luisa':'luisa',
- 'Edgar':'edgar',
- 'Ivan B':'ivan_b',
- 'Ben':'ben',
- 'Beto':'beto',
- 'Miguel B':'beto_b',
- 'Ilse':'ilse',
- 'Yosell':'yosell',
- 'Jose':'jose',
- 'Rodolfo':'rodolfo',
- 'Angelo':'angelo',
- 'Noe':'noe',
- 'Mike':'mike',
- 'Laila':'laila',
- 'Ana':'ana',
- 'Frank':'frank',
- 'Hugo':'hugo',
- 'Maria':'maria',
- 'Josue':'josue',
- 'Luis B':'luis_b',
- 'Donovan':'donovan',
- 'Miguel A':'miguel_a',
- 'Billy':'billy',
- 'Yair':'yair',
- 'Juan A':'juan_a',
- 'Laura B':'laura_b',
- 'Daniel':'daniel',
- 'Pablo':'pablo',
- 'Miguel C':'miguel_c',
- 'Yordani':'yordani',
- 'Daniel B':'daniel_b',
- 'Sylent':'sylent',
- 'Carolina':'carolina',
- 'Rodrigo':'rodrigo',
- 'Lucero':'lucero',
- 'Luis A':'luis_a',
- 'Zayuri':'zayuri',
- 'Ivan':'ivan',
- 'Axel':'axel',
- 'Ruben':'ruben',
- 'Jesus C':'jesus_c',
- 'Laura A':'laura_a',
- 'Roberto':'roberto',
- 'Carlos':'carlos',
- 'Cuauhtémoc':'cuauhtemoc',
- 'Juan B':'juan_b',
- 'Diana':'diana',
- 'Brenda':'brenda',
- 'Valeria':'valeria',
- 'Many':'many',
- 'Olimpya':'olimpya',
- 'Joana':'joana',
- 'Melani':'melani',
- 'Angel':'angel',
- 'Jeimmy':'jeimmy',
- 'Cris':'cris',
- 'Rocio':'rocio',
- 'Kevin':'kevin',
- 'Julio Cesar':'julio'}
+names_files = {'Joseph': 'joseph',
+               'Jesus A': 'jesus_a',
+               'Erik': 'erik',
+               'Abel': 'abel',
+               'Luisa': 'luisa',
+               'Edgar': 'edgar',
+               'Ivan B': 'ivan_b',
+               'Ben': 'ben',
+               'Beto': 'beto',
+               'Miguel B': 'beto_b',
+               'Ilse': 'ilse',
+               'Yosell': 'yosell',
+               'Jose': 'jose',
+               'Rodolfo': 'rodolfo',
+               'Angelo': 'angelo',
+               'Noe': 'noe',
+               'Mike': 'mike',
+               'Laila': 'laila',
+               'Ana': 'ana',
+               'Frank': 'frank',
+               'Hugo': 'hugo',
+               'Maria': 'maria',
+               'Josue': 'josue',
+               'Luis B': 'luis_b',
+               'Donovan': 'donovan',
+               'Miguel A': 'miguel_a',
+               'Billy': 'billy',
+               'Yair': 'yair',
+               'Juan A': 'juan_a',
+               'Laura B': 'laura_b',
+               'Daniel': 'daniel',
+               'Pablo': 'pablo',
+               'Miguel C': 'miguel_c',
+               'Yordani': 'yordani',
+               'Daniel B': 'daniel_b',
+               'Sylent': 'sylent',
+               'Carolina': 'carolina',
+               'Rodrigo': 'rodrigo',
+               'Lucero': 'lucero',
+               'Luis A': 'luis_a',
+               'Zayuri': 'zayuri',
+               'Ivan': 'ivan',
+               'Axel': 'axel',
+               'Ruben': 'ruben',
+               'Jesus C': 'jesus_c',
+               'Laura A': 'laura_a',
+               'Roberto': 'roberto',
+               'Carlos': 'carlos',
+               'Cuauhtémoc': 'cuauhtemoc',
+               'Juan B': 'juan_b',
+               'Diana': 'diana',
+               'Brenda': 'brenda',
+               'Valeria': 'valeria',
+               'Many': 'many',
+               'Olimpya': 'olimpya',
+               'Joana': 'joana',
+               'Melani': 'melani',
+               'Angel': 'angel',
+               'Jeimmy': 'jeimmy',
+               'Cris': 'cris',
+               'Rocio': 'rocio',
+               'Kevin': 'kevin',
+               'Julio Cesar': 'julio'}
 
 
 def hypothesis_grabber(interview_name, offset):
@@ -119,12 +119,12 @@ def search_ignore_space(pattern_text, string):
     return [matched_string, start, end]
 
 
-def collect_annotations(interview_name:str):
+def collect_annotations(interview_name: str):
     """This program re-formats the annotations from a *specific* hypothesis interview into a list of dictionaries that
     align more closely with the Spacy auto-generated annotations."""
 
     # asking for an interview name to append to end of URL
-    interview_name = interview_name #input("Enter name of Interview (spaces are underscores): ")
+    interview_name = interview_name  # input("Enter name of Interview (spaces are underscores): ")
     interview_name = str(interview_name).lower()
 
     # finding the number of annotations
@@ -163,7 +163,7 @@ def collect_annotations(interview_name:str):
     if raw_text_document.status_code == 200:
         raw_text_document = raw_text_document.json()
         raw_text_document = raw_text_document.get('text')
-    else: # error loading json file
+    else:  # error loading json file
         return "error"
     # defining a list of un-parsed annotations
     unparsed_annotations = []
@@ -217,13 +217,13 @@ def collect_annotations(interview_name:str):
                     pass
         except Exception as e:
             print(e)
-    
+
     # putting our pre-defined index themes into a list
     index_themes = open("../index_themes.txt").read().splitlines()
 
     # importing our semantic search vocabulary
     nlp = spacy.load("en_core_web_lg")
-    
+
     # defining a list of finalized annotations
     annotations = []
 
@@ -270,7 +270,7 @@ def collect_annotations(interview_name:str):
                 # if the annotation is at least an 85% match, we replace it with its matched index theme
                 if index_fuzzy_matcher[1] >= 85:
                     matched_annotation = matched_annotation + index_fuzzy_matcher[0] + ", "
-                    matched.append([index_split[n], index_fuzzy_matcher[0]]) # this line is for testing
+                    matched.append([index_split[n], index_fuzzy_matcher[0]])  # this line is for testing
 
                 # if the annotation is less than an 85% match, we attempt a semantic search
                 else:
@@ -287,13 +287,15 @@ def collect_annotations(interview_name:str):
 
                     if max_semantic_similarity >= 70:  # a 70% confidence tells us to match our annotation
                         matched_annotation = matched_annotation + most_similar_index + ", "
-                        semanticmatched.append([index_split[n], most_similar_index, max_semantic_similarity]) # this line is for testing
+                        semanticmatched.append(
+                            [index_split[n], most_similar_index, max_semantic_similarity])  # this line is for testing
 
                     # if neither the fuzzy search nor the semantic search return anything, we simply append the
                     # original term
                     else:
                         matched_annotation = matched_annotation + index_split[n] + ", "
-                        unmatched.append([index_split[n], most_similar_index, max_semantic_similarity]) # this line is for testing
+                        unmatched.append(
+                            [index_split[n], most_similar_index, max_semantic_similarity])  # this line is for testing
 
             matched_annotation = matched_annotation[:-2]  # removing the final comma
 
@@ -310,7 +312,7 @@ def collect_annotations(interview_name:str):
 
             # appending this parsed annotation to our list of finalized annotations
             annotations.append(parsed_annotation)
-    
+
     # FOR DEBUGGING OUR MATCHER
     # print(annotations)
     # print(matched)
@@ -319,7 +321,9 @@ def collect_annotations(interview_name:str):
     return annotations
 
 
-def load_data(): 
+def load_data():
+    """This program takes every interview in the data directory, runs collect_annotations on each one, and then
+    returns a list of dictonaries, each of which corresponds to a specific interview."""
     all_data = []  # initializing our list
     interviews = list(Path('../data').iterdir())  # iterating over our /data folder
     for interview in tqdm(interviews):
@@ -331,15 +335,16 @@ def load_data():
     return all_data
 
 
-def make_csv(data:List[dict]):
-    df_data = [] 
-    for row in data: 
-        for i in  row['annotations']:  
-            if isinstance(i,str): 
-                pass 
-            else: 
-                i['name'] = row['name'] 
-                i['text'] = row['text'][i['start']:i['end']] 
+def make_csv(data: List[dict]):
+    """This program takes the output of load_data() and re-formats it into a .csv file"""
+    df_data = []
+    for row in data:
+        for i in row['annotations']:
+            if isinstance(i, str):
+                pass
+            else:
+                i['name'] = row['name']
+                i['text'] = row['text'][i['start']:i['end']]
                 df_data.append(i)
     df = pd.DataFrame(df_data)
     return df
